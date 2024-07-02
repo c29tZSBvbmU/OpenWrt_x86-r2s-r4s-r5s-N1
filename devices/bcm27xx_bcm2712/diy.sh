00b81df70c58380b02510d29473c2bcb7a68d75f
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2024-07-03 00:36:03 +0800

    update

--- devices/bcm27xx_bcm2712/diy.sh
+++ devices/bcm27xx_bcm2712/diy.sh
@@ -4,4 +4,6 @@ SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
 bash $SHELL_FOLDER/../common/kernel_6.6.sh
 
+rm -rf package/devel/kselftests-bpf
+
 sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += fdisk lsblk kmod-usb-net-asix-ax88179 kmod-usb-net-rtl8152/' target/linux/bcm27xx/Makefile
